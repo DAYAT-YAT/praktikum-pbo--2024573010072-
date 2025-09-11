2b5fd9f41eb60d2b04956574979980c6a780f463
dayatyat <rahmadhiidayat833@gmail.com>
2025-09-11 15:35:03 +0700

    iniasisasi

---
+++ src/modul_1/Main.java
@@ -0,0 +1,8 @@
+package model_1;
+
+public class Main {
+    //funsi khusus sebagai entry point dari aplikasi yang dibuat
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
